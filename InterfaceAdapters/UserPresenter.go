78a7b9db7781ab3aad27d74bb2a33e7ecd4003a7
Shawn Koh <shawnkoh@me.com>
2020-05-28 16:31:23 +0800

    Fix vms

--- InterfaceAdapters/UserPresenter.go
+++ InterfaceAdapters/UserPresenter.go
@@ -2,8 +2,10 @@ package interfaceadapters
 
 import "../usecases"
 
+// UserPresenter is responsible for presenting a User
 type UserPresenter struct{}
 
+// Present translates the CreateUserOutputData into a UserViewModel
 func (presenter UserPresenter) Present(output usecases.CreateUserOutputData) {
-
+	_ = UserViewModel{ID: output.ID, Username: output.Username}
 }
